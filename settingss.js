c04b024d79669804e96c983f7114aa1d7f719f8e
aimeakakpo38-maker <aimeakakpo38@gmail.com>
2025-09-08 16:48:43 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~k2VAgACA#zwsEh5YtJaHubVXVkCHZZRjUizvZlfI8qEpDm9gmdic",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "22896275402", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
